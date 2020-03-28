bc95e1e7f4a9387082f3d2d434cb51defae61210
craigfbarry <craigfbarry@gmail.com>
2020-03-28 21:37:34 +1030

    Left page divs to be static and add the content. Added 2nd query to get UV index.

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ $(document).ready(function(){
 
 
 $(".btn").on("click",function(){
-    var city = "sydney";
+    var city = "Sydney";
     console.log(city);
     var api_key = "181156e75b504c175179653b4b25401f";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&forecast?q=" 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&forecast?q="
     + city + "&appid="+ api_key;
     $.ajax({
         url:    queryURL,
         method: "GET"
     }).then(function(response){
-        var result = response.main.temp -273.15;
-        var humidity = response.main.humidity;
-        var wind = response.wind.speed;
+        var dateToday = moment().format("DD/MM/YYYY");    
+        var result = (response.main.temp -273.15).toFixed(1);
 
-        console.log(result);
-        console.log(humidity+" %");
-        console.log("Wind Speed:" + wind + "MPH")
+
+        $("#city-temperature").html("<h3>" + city + " " + dateToday + "</h3>");
+        $("#currentTemp").html("Temperature: " + result + "&#8451;");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
+
+        var UVqueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat+"&lon=" + response.coord.lon + "&appid=181156e75b504c175179653b4b25401f";
+        $.ajax({
+            url:    UVqueryURL,
+            method: "GET"
+        }).then(function(data){
+            console.log(data);
+            $("#UV-index").text("UV-index: " + data.value);
+        });
+       
     });
 
 });
