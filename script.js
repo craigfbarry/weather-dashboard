e00ae50c6544232b221d43ac4142a7ee16a0b509
craigfbarry <craigfbarry@gmail.com>
2020-03-29 13:44:17 +1030

    Applied ID's to city list and set as button inputs. Tested OK.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 
+//localStorage.getItem("savedCity");
+
+
+
+
+
+
+
 
 
 
 $(".btn").on("click",function(){
-    var city = "Sydney";
+    //Determine the city selected
+
+    var city = $(this).attr("id");
     console.log(city);
-    var api_key = "181156e75b504c175179653b4b25401f";
+    const api_key = "181156e75b504c175179653b4b25401f";
+    //API query which is determined by city chosen
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&forecast?q="
     + city + "&appid="+ api_key;
+    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid="+ api_key;
     $.ajax({
         url:    queryURL,
         method: "GET"
@@ -16,12 +28,13 @@ $(".btn").on("click",function(){
         var dateToday = moment().format("DD/MM/YYYY");    
         var result = (response.main.temp -273.15).toFixed(1);
 
-
+    //Display outputs to the city-temperature div
         $("#city-temperature").html("<h3>" + city + " " + dateToday + "</h3>");
         $("#currentTemp").html("Temperature: " + result + "&#8451;");
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
         $("#windSpeed").text("Wind Speed: " + response.wind.speed + "MPH");
 
+    //The UV-index requires an additional AJAX call dependant on latitude and longitude from the previous query
         var UVqueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat+"&lon=" + response.coord.lon + "&appid=181156e75b504c175179653b4b25401f";
         $.ajax({
             url:    UVqueryURL,
@@ -33,6 +46,25 @@ $(".btn").on("click",function(){
        
     });
 
+    $.ajax({
+        url:    queryForecastURL,
+        mthod:  "GET"
+    }).then(function(response){
+
+        //var forecastDay = $("div");
+
+
+
+        //$("#5-day-forecast").append(forecastDay)
+        console.log(response.list[2].dt_txt);
+        console.log(response.list[2].weather[0].icon);
+        console.log(response.list[2].main.temp_max);
+        console.log(response.list[2].main.humidity);
+
+    
+    })
+
+//localStorage.setItem("savedCity"+ city);
 });
 
 
